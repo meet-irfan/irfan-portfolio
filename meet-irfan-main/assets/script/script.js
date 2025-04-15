5b4978de2fd318223a7c2d504cf01a8f3cb3719e
meet-irfan <irfanali.bscsf21kdk@iba-suk.edu.pk>
2025-04-16 00:08:12 +0500

    Personal-Portfolio

---
+++ meet-irfan-main/assets/script/script.js
@@ -0,0 +1,53 @@
+const texts = ["Irfan Ali Narejo", "Data Engineer ", "Machine Learing Developer"];
+let textIndex = 0;
+let charIndex = 0;
+const speed = 100;
+
+
+function typeEffect() {
+    if ( charIndex < texts[textIndex].length ) {
+        document.getElementById("typed-text").textContent += texts[textIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(typeEffect, speed);
+    }
+    else {
+        setTimeout(eraseEffect, 1500);
+    }
+}
+
+
+function eraseEffect() {
+    if (charIndex > 0) {
+        document.getElementById("typed-text").textContent = texts[textIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(eraseEffect, speed/2);
+    }
+    else {
+        textIndex = (textIndex+1) % texts.length;
+        setTimeout(typeEffect, speed);
+    }
+}
+
+window.onload = typeEffect;
+
+
+/* Dark Mode Toggle */
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+const info = document.getElementById("info");
+const social_icons = document.getElementById("social-icons");
+const icon = darkModeToggle.querySelector("i");
+
+darkModeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    // Toggle between moon and sun icon
+    if (document.body.classList.contains("dark-mode")) {
+        icon.classList.replace("fa-moon", "fa-sun");
+        info.classList.add("info");
+        social_icons.classList.add("social");
+    } else {
+        icon.classList.replace("fa-sun", "fa-moon");
+        info.classList.remove("info");
+        social_icons.classList.remove("social");
+    }
+});
